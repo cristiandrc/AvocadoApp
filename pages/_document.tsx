d9b98f7cef17e45ba6a92d1ccd5ce5901cf2e8e6
Cristian Rojas <crisdarroca@hotmail.com>
2023-02-15 21:35:30 -0500

    feat: estendiendo el document del html

---
+++ pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>{/* Favicon - fuentes - url Styles - scripts */}</Head>
+      <body className="my-body-class">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
